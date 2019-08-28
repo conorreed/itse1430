aec170291cbef31af9cffd3de9ace1df2b98ca54
conor.reed <conor.reed@NEITD-211208>
2019-08-28 17:55:43 -0500

    stuff

--- classwork/Section1/HelloWorld/Program.cs
+++ classwork/Section1/HelloWorld/Program.cs
@@ -4,11 +4,44 @@ namespace HelloWorld
 {
     class Program
     {
-        static void Main ( string[] args )
+        static char Main ( /*string[] args*/ )
         {
-            Console.WriteLine ("Hello World!");
+          // movie data
+          string title;
+            int runLength;
+            int releaseYear;
+            string description;
+            bool haveSeen;
+
+            DisplayMenu ();
         }
 
+        static char DisplayMenu ()
+        {
+            do
+            {
+                 Console.WriteLine("A)dd Movie");
+                 Console.WriteLine("Q)uit");
 
+            string input = Console.ReadLine();
+            if (input == "A")
+            {
+                    return 'A';
+            } else if (input == "Q")
+            {
+                    return 'Q';
+            } else
+            {
+                Console.WriteLine("Invalid Input");
+            }
+
+            } while(true);
+           
+           
+        }           
+
+   
+        
+        
     }
 }
